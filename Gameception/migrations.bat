783802f434377de687c2c51ad3a70e6cdba046e7
pns2712 <pns.2712@gmail.com>
2016-05-27 17:14:23 -0300

    Add files via upload

---
+++ Gameception/migrations.bat
@@ -0,0 +1,6 @@
+python manage.py makemigrations Assinante
+python manage.py makemigrations Acervo
+python manage.py makemigrations InfoGeral
+python manage.py migrate Assinante
+python manage.py migrate Acervo
+python manage.py migrate InfoGeral
