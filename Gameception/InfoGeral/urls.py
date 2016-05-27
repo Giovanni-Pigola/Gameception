ef84f09a48997890fba7c391f2e7cc474374639b
Jay-42 <giovanni.pigola.mannucci@gmail.com>
2016-05-27 16:54:12 -0300

    Add files via upload

--- Gameception/InfoGeral/urls.py
+++ Gameception/InfoGeral/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index, name = "home"),
-    url(r'^Home/', views.index, name = "home"),
-    url(r'^Sobre/', views.Sobre, name = "Sobre"),
-    url(r'^Contatos/', views.Contatos, name = "Contatos")
+    url(r'^$', views.index, name = "home"),                #acesso inicial da url e pelo botão home
+    url(r'^Sobre', views.Sobre, name = "Sobre"),           #acesso a pagina de Sobre
+    url(r'^Contatos', views.Contatos, name = "Contatos")   #acesso a pagina de Contatos
 ]
