6a3a4c9431336f074388fe7dcd60666b6a970128
Jay-42 <giovanni.pigola.mannucci@gmail.com>
2016-06-08 10:09:42 -0300

    Add files via upload

--- Gameception/InfoGeral/views.py
+++ Gameception/InfoGeral/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render (request, 'MainPage.html', {})
+    return render (request, 'Infos/Home.html', {})
 
 def Sobre(request):
     return render (request, 'Infos/Sobre.html', {})
