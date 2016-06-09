b5188ef72204fcd9eb42c8985d35fcb131db6c96
IanELang <ianelang2@gmail.com>
2016-06-09 13:34:10 -0300

    Add files via upload

--- Gameception/Acervo/views.py
+++ Gameception/Acervo/views.py
@@ -5,16 +5,14 @@ from Assinante.models import Jogo
 from Assinante.models import Genero
 # Create your views here.
 def Acervo(request):
+    nomeFiltro = None
     jogos = Jogo.objects.order_by('-id')
     generos = Genero.objects.all()
-    filtrado = False #indica se foi clicado em algum dos generos pra filtrar
-    return render(request, 'Acervo/Acervo.html', {'generos' : generos, 'jogos' : jogos, 'filtrado' : filtrado})
+    return render(request, 'Acervo/Acervo.html', {'generos' : generos, 'jogos' : jogos, 'nomeFiltro' : nomeFiltro})
 
 def filtro(request, genero):
-    filtrado = True
-    gen = "RPG" #coloquei como rpg pra teste. ele filtra certinho. mas pelo visto, ele nao ta pegando o 'genero' ainda na funcao
-    #se poe gen = str(genero), ele pega '' (vi isso quando tava dando outros erros na hora de implementar o filtro)
-    #arrumado esse detalhe, o fltro funciona
+    nomeFiltro = "Jogos de " + genero
     generos = Genero.objects.all()
-    jogos = Jogo.objects.order_by('-id')
-    return render(request, 'Acervo/Acervo.html', {'generos' : generos, 'jogos' : jogos, 'gen' : gen , 'filtrado' : filtrado})
+    generoEscolhido = Genero.objects.get(nome=genero)
+    jogos = generoEscolhido.jogo_set.all().order_by('-id')
+    return render(request, 'Acervo/Acervo.html', {'generos' : generos, 'jogos' : jogos, 'nomeFiltro' : nomeFiltro})
