3dac962e39b9a5f0722e77d4dedf38ac7624a8fd
GMierzwa <guilherme_mierzwa@hotmail.com>
2016-06-19 16:15:37 -0300

    Add files via upload

--- Gameception/Acervo/urls.py
+++ Gameception/Acervo/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.Acervo, name = "Acervo"),
     #url(r'^/AcervoFiltro/', views.filtro, name="AcervoFiltro"),
-    url(r'^/(?P<genero>\w+)/', views.filtro, name="AcervoFiltro"),
+    url(r'^/(?P<genero>[-\w ]+)/', views.filtro, name="AcervoFiltro"),
 ]
