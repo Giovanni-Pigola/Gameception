45480fb33c99e2428949714d6e47d751560a14e9
GMierzwa <guilherme_mierzwa@hotmail.com>
2016-06-19 18:19:24 -0300

    Add files via upload

--- Gameception/Acervo/views.py
+++ Gameception/Acervo/views.py
@@ -10,11 +10,24 @@ def Acervo(request):
     generos = Genero.objects.all()
     return render(request, 'Acervo/Acervo.html', {'generos' : generos, 'jogos' : jogos, 'filtrado' : filtrado})
 
-def filtro(request, genero):
-    filtrado = True
+def filtro(request, genero, pagina):
+    pagina = int(pagina)
+    if(genero == 'Todos'):
+        listajogos = Jogo.objects.order_by('-id')
+        jogos = []
+        for i in range(99*(pagina-1), 99*pagina):
+            try:
+                jogos.append(listajogos[i])
+            except:
+                pass
+    else:
+        listajogos = Jogo.objects.filter(listaGeneros__nome=genero).order_by('-id')
+        jogos = []
+        for i in range(99*(pagina-1), 99*pagina):
+            try:
+                jogos.append(listajogos[i])
+            except:
+                pass
+    paginas = len(listajogos)/99
     generos = Genero.objects.all()
-    gen = genero
-    jogos = Jogo.objects.order_by('-id')
-    #generoEscolhido = Genero.objects.get(nome=genero)
-    #jogos = generoEscolhido.jogo_set.all().order_by('-id')
-    return render(request, 'Acervo/Acervo.html', {'generos' : generos, 'jogos' : jogos, 'filtrado' : filtrado, 'gen' : gen})
+    return render(request, 'Acervo/Acervo.html', {'generos' : generos, 'jogos' : jogos, 'paginas': paginas})
